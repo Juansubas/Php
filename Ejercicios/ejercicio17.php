6dbf2fd277acf555c0de556cfffc43e044455b59
juansubas <juansubas@gmail.com>
2023-05-26 18:45:05 -0500

    Functions

---
+++ Ejercicios/ejercicio17.php
@@ -0,0 +1,12 @@
+<?php
+
+//Functions
+function ImprimirNombre($Nombre, $Apellido=""){
+    //Rutinas (Instrucciones)
+    echo "Hola ".$Nombre." ".$Apellido."<br/>";
+}
+
+ImprimirNombre("Oscar");
+ImprimirNombre("Pedro","Perez");
+ImprimirNombre("Juan","Garcia");
+?>
