1d3795034ea638434cdc356ed28a0d551a7e5d7f
juansubas <juansubas@gmail.com>
2023-05-26 00:10:14 -0500

    Constantes, variables y mas

---
+++ Ejercicios/Ejercicio7.php
@@ -0,0 +1,18 @@
+<?php
+
+$edad=12; //Variables
+
+echo $edad."<br/>";
+
+$edad=48; // Cambiar valor de la variable
+
+echo $edad."<br/>";
+
+//uso de constante
+
+define("NOMBRE","OSCAR");
+
+//Imprimir constante
+echo NOMBRE;
+
+?>
---
+++ Ejercicios/ejercicio6.php
@@ -0,0 +1,16 @@
+<?php
+
+/*Tipos de datos
+No hay que decir a que tipo
+*/
+
+$Edad = 32; // Entero
+
+$Nombre = "Oscar"; // String
+
+$Altura = 1.4; // Flotante o Decimal
+
+$Programador = null; // Valor Nulo
+
+echo " El nombre es ".$Nombre." la edad es ".$Edad." su altura es ".$Altura." el puesto ".$Programador;
+?>
