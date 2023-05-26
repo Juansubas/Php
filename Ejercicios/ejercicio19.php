3d9ea4e5235e64051f4e802f2df9d10acc1a5d31
juansubas <juansubas@gmail.com>
2023-05-26 18:56:30 -0500

    Function Strings

---
+++ Ejercicios/ejercicio19.php
@@ -0,0 +1,16 @@
+<?php
+
+//Functions string php
+//https://www.php.net/manual/en/ref.strings.php
+
+$nombre= "banco republica";
+
+$nombreMayusculas = strtoupper($nombre);
+
+$nombreMinusculas = strtolower($nombreMayusculas);
+
+echo "Nombre en mayusculas: ".$nombreMayusculas. "<br/>";
+
+echo "Nombre en Minusculas: ".$nombreMinusculas. "<br/>";
+
+?>
