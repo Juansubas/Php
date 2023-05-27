095d00c3a8afbf875fda8e3d7ae6a8baf5eab4d0
juansubas <juansubas@gmail.com>
2023-05-26 19:00:47 -0500

    Date Functions

---
+++ Ejercicios/ejercicio20.php
@@ -0,0 +1,11 @@
+<?php
+//Date Functions
+
+//Puedo mostrar un formato como quiera para mas info
+//https://www.php.net/manual/en/function.date.php
+$hoy = date("Y - m - d");
+$hoy = date("Y / m / d");
+
+echo $hoy;
+
+?>
