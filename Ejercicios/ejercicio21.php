01b40c636da8a1eacf52f993c05ef2cf550b0972
juansubas <juansubas@gmail.com>
2023-05-26 19:04:10 -0500

    Creando Indices de Array

---
+++ Ejercicios/ejercicio21.php
@@ -0,0 +1,11 @@
+<?php
+
+//Creamos array con la palabra
+$frutas = array("fresa", "Pera", "Manzana");
+
+//lo vamos a leer con print_r
+//pues ayuda a imprimir la estructura de una variable
+
+print_r($frutas);
+
+?>
