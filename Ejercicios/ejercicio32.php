a23ab216565c91ede50b73e386476d078774cd20
juansubas <juansubas@gmail.com>
2023-05-28 10:11:23 -0500

    Guardar archivos con php

--- Ejercicios/ejercicio32.php
+++ Ejercicios/ejercicio32.php
@@ -6,6 +6,13 @@ if($_POST){
     //Para manipular el file
     //Debemos poner $_FILES nombre del input y nombre de la prop a obtener
     print_r($_FILES['archivo']['name']);
+
+    //Obtener el archiv ooriginal
+    //tmp_name es el nombre original que se ve en el arreglo archivo
+    //primer argumento guardar el nombre temporal y el segundo el nombre real.
+    //basicamente cuando subimos archivos se almacenan temporalmente hasta moverlos definitivo
+    //por lo tanto necesitamos el temporal para volverlo normal.
+    move_uploaded_file($_FILES['archivo']['tmp_name'],$_FILES['archivo']['name']);
 }
 
 ?>
