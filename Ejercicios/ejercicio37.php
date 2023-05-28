f3a732a5a652d3b3f54fc9822caa401260f1e440
juansubas <juansubas@gmail.com>
2023-05-28 11:40:02 -0500

    Usando Require para incluir codigo

---
+++ Ejercicios/ejercicio37.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"
+    <title>Document</title>
+</head>
+<body>
+<!-- Usaremos require que aunque se parece a include
+hay una diferencia-->
+
+<?php require('ejercicio37_1.php'); ?>
+
+</body>
+</html>
