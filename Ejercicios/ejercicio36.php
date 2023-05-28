30ecbdb4a079b7f53b92c1f85c1df6f1ec819ad3
juansubas <juansubas@gmail.com>
2023-05-28 11:29:51 -0500

    Usando Include para incluir codigo de otros archivos php

---
+++ Ejercicios/ejercicio36.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chatset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio web</title>
+</head>
+<body>
+    <?php include 'ejercicio36_1.php' ?>
+</body>
+
+</html>
