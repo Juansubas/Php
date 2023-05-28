5c604d893465f78054f439d7877c7eacbde23d62
juansubas <juansubas@gmail.com>
2023-05-28 11:12:37 -0500

    Consumiendo api get y modificando data

--- Ejercicios/ejercicio35.php
+++ Ejercicios/ejercicio35.php
@@ -20,7 +20,19 @@ $opciones = array("ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false));
 //haremos ningun procesamiento extra en el contenido
 $respuesta = file_get_contents($url, false, stream_context_create($opciones));
 
-$objRespuesta = json_encode($respuesta);
+$objRespuesta = json_decode($respuesta);
 
-print_r($objRespuesta);
+//print_r($objRespuesta);
+
+foreach($objRespuesta->list as $video){
+    //print_r($video->title);
+    //print_r($video->channel);
+}
+
 ?>
+
+<ul>
+    <?php foreach($objRespuesta->list as $video){ ?>
+        <li><?php echo ($video->title); ?> | <?php echo ($video -> channel); ?></li>
+    <?php } ?>
+</ul>
