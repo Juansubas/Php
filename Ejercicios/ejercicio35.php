154452dba18fbcab64ff267e03aa9b3540746cbc
juansubas <juansubas@gmail.com>
2023-05-28 11:02:46 -0500

    Consumiendo una api en php

---
+++ Ejercicios/ejercicio35.php
@@ -0,0 +1,26 @@
+<?php
+
+//Consumir una API con php
+//https://developers.dailymotion.com/
+
+$url = "https://api.dailymotion.com/videos?channel=sport&limit=10";
+
+//Opciones para consumir la api, necesitamos por ejemplo ssl para el https
+//Con ssl le indicamos que vamos a modificar esto
+//primer parametro es para verificar el certificado ssl
+//segundo para validar el nombre del servidor en el certificado
+//de esta manera al desactivar esta seguridad pues vamos mas facil
+//peer se refeire al extremo o servidor
+$opciones = array("ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false));
+
+//leer contenido de una url, stream_context_create, con false indicamos que no
+//stream_context_create es para crear un contexto de transmision para pasarle
+//las opciones que debera tener en cuenta al leer la url o la solicitud
+//en este cado la conexion SSL que no la tendremos en cuenta
+//haremos ningun procesamiento extra en el contenido
+$respuesta = file_get_contents($url, false, stream_context_create($opciones));
+
+$objRespuesta = json_encode($respuesta);
+
+print_r($objRespuesta);
+?>
