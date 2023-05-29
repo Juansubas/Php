83967e22bcaac24a404fa70e3e85a9d432f66585
juansubas <juansubas@gmail.com>
2023-05-29 16:57:28 -0500

    Api finalizada

--- API/productsController.php
+++ API/productsController.php
@@ -17,14 +17,14 @@ switch ($_SERVER['REQUEST_METHOD']){
         //en este caso especificamos con php://input que recibe por post en formato string
         //el true es para indicar que procese el resutlado como array, no es obligatorio pero por defecto es false
             $_POST = json_decode(file_get_contents('php://input',true));
-        if(!isset($_POST->name) || is_null($_POST->name) || empty(trim($_POST->name)) ){
-            $respuesta = ['error', 'El nombre del producto no debe estar vacio'];
+        if(!isset($_POST->name) || is_null($_POST->name) || empty(trim($_POST->name)) || strlen($_POST->name) > 80 ){
+            $respuesta = ['error', 'El nombre del producto no debe estar vacio y no debe tener mas de 80 caracteres'];
         }
-        else if(!isset($_POST->description) || is_null($_POST->description) || empty(trim($_POST->description)) ){
-            $respuesta = ['error', 'La descripcion del producto no debe estar vacia'];
+        else if(!isset($_POST->description) || is_null($_POST->description) || empty(trim($_POST->description)) || strlen($_POST->name) > 150 ){
+            $respuesta = ['error', 'La descripcion del producto no debe estar vacia, no debe tener mas de 150 caracteres y no debe tener mas de 150 caracteres'];
         }
-        else if(!isset($_POST->price) || is_null($_POST->price) || empty(trim($_POST->price)) ){
-            $respuesta = ['error', 'El precio del producto no debe estar vacio'];
+        else if(!isset($_POST->price) || is_null($_POST->price) || empty(trim($_POST->price)) || !is_numeric($_POST->price) || strlen($_POST->price) > 20 ){
+            $respuesta = ['error', 'El precio del producto no debe estar vacio y debe ser de tipo numerico'];
         }else{
             $respuesta = $productsModel->saveProducts($_POST->name,$_POST->description,$_POST->price);
         }
@@ -37,14 +37,14 @@ switch ($_SERVER['REQUEST_METHOD']){
         if(!isset($_PUT->id) || is_null($_PUT->id) || empty(trim($_PUT->id)) ){
             $respuesta = ['error', 'El ID del producto no debe estar vacio'];
         }
-        if(!isset($_PUT->name) || is_null($_PUT->name) || empty(trim($_PUT->name)) ){
-            $respuesta = ['error', 'El nombre del producto no debe estar vacio'];
+        if(!isset($_PUT->name) || is_null($_PUT->name) || empty(trim($_PUT->name)) || strlen($_POST->name) > 80){
+            $respuesta = ['error', 'El nombre del producto no debe estar vacio y no debe tener mas de 80 caracteres'];
         }
-        else if(!isset($_PUT->description) || is_null($_PUT->description) || empty(trim($_PUT->description)) ){
-            $respuesta = ['error', 'La descripcion del producto no debe estar vacia'];
+        else if(!isset($_PUT->description) || is_null($_PUT->description) || empty(trim($_PUT->description)) || strlen($_POST->name) > 150){
+            $respuesta = ['error', 'La descripcion del producto no debe estar vacia y no debe tener mas de 150 caracteres'];
         }
-        else if(!isset($_PUT->price) || is_null($_PUT->price) || empty(trim($_PUT->price)) ){
-            $respuesta = ['error', 'El precio del producto no debe estar vacio'];
+        else if(!isset($_PUT->price) || is_null($_PUT->price) || empty(trim($_PUT->price)) || !is_numeric($_PUT->price || strlen($_PUT->price) > 20) ){
+            $respuesta = ['error', 'El precio del producto no debe estar vacio, debe ser de tipo numerico y no contener mas de 20 caracteres'];
         }else{
             $respuesta = $productsModel->updateProducts($_PUT->id,$_PUT->name,$_PUT->description,$_PUT->price);
         }
--- API/productsModel.php
+++ API/productsModel.php
@@ -7,7 +7,7 @@ class productsModel{
         mysqli_set_charset($this->conexion, 'utf8');
     }
 
-    public function getProducts(){
+    public function getProducts($id=null){
         //validacion de la conexion
         //la propiedad connect_errno retorna el numero del error y en if un numero diferente de cero
         //es true activando el condicional
@@ -17,8 +17,9 @@ class productsModel{
             echo "Error en la conexión: " . $this->conexion->connect_error;
             return "";
         }
+        $where = ($id == null) ? "" : " WHERE id='$id'";
         $products=[];
-        $sql="SELECT * FROM products";
+        $sql="SELECT * FROM products ".$where;
         //Llega unos valores despues de la consulta
         $registros = mysqli_query($this->conexion,$sql);
         //con assoc transforma las filas en un array asociativo
@@ -34,30 +35,58 @@ class productsModel{
     }
 
     public function saveProducts($name, $description, $price){
-        $sql="INSERT INTO products(name,description,price) VALUES('$name', '$description', '$price')";
         if ($this->conexion->connect_errno) {
             //con connect_error tenemos el mensaje de error.
             echo "Error en la conexión: " . $this->conexion->connect_error;
             return "";
         }
-        mysqli_query($this->conexion,$sql);
-        $resultado=["Success","Producto Guardado"];
+        $valida = $this->validateProducts($name, $description, $price);
+        $resultado = ['error', 'Ya existe un producto con las mismas caracteristicas'];
+        if(count($valida)==0){
+            $sql="INSERT INTO products(name,description,price) VALUES('$name', '$description', '$price')";
+            mysqli_query($this->conexion,$sql);
+            $resultado=["Success","Producto Guardado"];
+        }
+
         return $resultado;
 
     }
 
     public function updateProducts($id, $name, $description, $price){
-        $sql = "UPDATE products SET name = '$name', description='$description', price='$price' WHERE id='$id'";
-        mysqli_query($this->conexion, $sql);
-        $resultado=["Success", "Producto Actualizado"];
+        $existe= $this->getProducts($id);
+        $resultado=['error','No existe el producto con ID'.$id];
+        if(count($existe)>0){
+            $valida = $this->validateProducts($name,$description,$price);
+            $resultado=['Error', 'Ya existe un producto con las mismas caracteristicas '];
+            if(count($valida)==0){
+                $sql = "UPDATE products SET name = '$name', description='$description', price='$price' WHERE id='$id'";
+                mysqli_query($this->conexion, $sql);
+                $resultado=["Success", "Producto Actualizado"];
+            }
+        }
+
         return $resultado;
     }
 
     public function deleteProducts($id){
-        $sql = "DELETE FROM products WHERE id = '$id'";
-        mysqli_query($this->conexion, $sql);
-        $resultado = ['Success', "Producto Eliminado"];
+        $valida = $this->getProducts($id);
+        $resultado = ['error', 'No existe el product con ID'.$id];
+        if(count($valida)>0){
+            $sql = "DELETE FROM products WHERE id = '$id'";
+            mysqli_query($this->conexion, $sql);
+            $resultado = ['Success', "Producto Eliminado"];
+        }
         return $resultado;
+    }
+
+    public function validateProducts($name, $description, $price){
+        $products=[];
+        $sql = "SELECT * FROM products WHERE name='$name'AND description = '$description' AND price='$price'";
+        $registros = mysqli_query($this->conexion, $sql);
+        while($row = mysqli_fetch_assoc($registros)){
+            array_push($products, $row);
+        }
+        return $products;
     }
 }
 
